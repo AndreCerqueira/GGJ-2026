93a3f4333d4589467a7632a5deb9e3c123a357e2
André Cerqueira <78262628+AndreCerqueira@users.noreply.github.com>
2026-02-01 03:09:12 +0000

    .

--- Assets/Andre/Scripts/PlayerView.cs
+++ Assets/Andre/Scripts/PlayerView.cs
@@ -1,5 +1,6 @@
 ﻿using Andre.Scripts.Systems;
 using UnityEngine;
+using Project.Runtime.Scripts.General; // <--- ADICIONA ISTO (Namespace do CursorManager)
 
 namespace Andre.Scripts
 {
@@ -8,7 +9,6 @@ namespace Andre.Scripts
         [SerializeField] private Light _playerLight;
         public Light PlayerLight => _playerLight;
         
-        // Static registry of players to make checks easier at runtime
         public static System.Collections.Generic.List<PlayerView> AllPlayers { get; } = new System.Collections.Generic.List<PlayerView>();
 
         private void OnEnable()
@@ -19,11 +19,29 @@ namespace Andre.Scripts
         private void OnDisable()
         {
             AllPlayers.Remove(this);
+            // Segurança: Se o jogador morrer/desaparecer enquanto o rato está em cima, reseta o cursor
+            if (CursorManager.Instance != null) CursorManager.Instance.SetDefaultCursor();
         }
 
         private void OnMouseDown()
         {
             AreaMovementSystem.Instance.SelectPlayer(this);
+        }
+
+        // --- ADICIONA ESTES MÉTODOS ---
+
+        private void OnMouseEnter()
+        {
+            // Quando o rato entra no jogador, muda para a "mãozinha"
+            if (CursorManager.Instance != null) 
+                CursorManager.Instance.SetInteractCursor();
+        }
+
+        private void OnMouseExit()
+        {
+            // Quando o rato sai, volta ao normal
+            if (CursorManager.Instance != null) 
+                CursorManager.Instance.SetDefaultCursor();
         }
     }
 }
