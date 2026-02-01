b1a437002c53c8ed1c09e3b5fa9cf0b44cbf30b6
André Cerqueira <78262628+AndreCerqueira@users.noreply.github.com>
2026-02-01 15:50:02 +0000

    .

---
+++ Assets/Andre/Scripts/ExitClickHandler.cs
@@ -0,0 +1,20 @@
+﻿using Andre.Scripts.Systems;
+using UnityEngine;
+
+namespace Andre.Scripts
+{
+    public class ExitClickHandler : MonoBehaviour
+    {
+        private void OnMouseDown()
+        {
+            // Procura o componente AreaView nos pais deste objeto
+            var area = GetComponentInParent<AreaView>();
+            
+            if (area != null)
+            {
+                // Simula o clique na área, permitindo o movimento
+                AreaMovementSystem.Instance.OnAreaClicked(area);
+            }
+        }
+    }
+}
